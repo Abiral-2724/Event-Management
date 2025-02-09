608fbab0bba5142502aec3c3e8cf3167e5fe34f3
Abiral-2724 <abiral2004jain@gmail.com>
2025-02-09 23:36:34 +0530

    developnment

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,12 @@ import connectDB from './utils/db.js';
 import userRoute from './routes/user.route.js';
 import eventRoute from './routes/event.route.js' ;
 dotenv.config({});
-
+import path from 'path'
 
 const PORT = process.env.PORT || 8000;
 const app = express() ;
  
+const _dirname = path.resolve() ;
 
 app.use(cors({
     origin: 'http://localhost:5173',
@@ -29,6 +30,11 @@ allowedHeaders: ['Content-Type', 'Authorization']
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/event", eventRoute);
+
+app.use(express.static(path.join(_dirname,"/frontend/dist")))
+app.get('*',(req,res) => {
+  res.sendFile(path.resolve(_dirname ,"frontend","dist","index.html"))
+})
 
 // Start server
 app.listen(PORT, () => {
